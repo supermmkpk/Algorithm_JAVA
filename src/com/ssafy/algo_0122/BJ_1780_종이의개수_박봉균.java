4f513295b8312e9b91967bd8f229cff0fc0896b9
supermmkpk <supermmkpk@gmail.com>
2024-01-22 20:10:07 +0900

    20240122

---
+++ src/com/ssafy/algo_0122/BJ_1780_종이의개수_박봉균.java
@@ -0,0 +1,24 @@
+package com.ssafy.algo_0122;
+
+/**
+ * <pre>
+ * BJ1780 : 종이의 개수
+ * </pre>
+ * 
+ * @author 박봉균
+ * @since JDK17
+ * @see <a href=""></a>
+ *
+ * 결과: 
+ */
+
+/*
+ */
+public class BJ_1780_종이의개수_박봉균 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
